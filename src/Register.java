76b8262e2214aa9adc2ba63234f84a6c0ed91758
VladimirsKrasikovs <108757262+VladimirsKrasikovs@users.noreply.github.com>
2022-08-01 22:47:45 +0300

    Update Register.java

--- src/Register.java
+++ src/Register.java
@@ -3,56 +3,59 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Registr extends JDialog {
+public class Register extends JFrame {
+
     private JTextField tfName;
     private JTextField tfEmail;
     private JTextField tfNumber;
     private JComboBox cbCountry;
     private JTextField tfCity;
-    private JTextField tfStreet;
     private JPasswordField tfPassword;
-    private JRadioButton maleRadioButton;
-    private JRadioButton femaleRadioButton;
+    private JRadioButton male;
+    private JRadioButton female;
     private JButton btnRegister;
     private JPanel Registration;
-    private JList list1;
-    private JTextField result;
+
+    private JTextField textField1;
+    private JTextArea text1;
+
+    private JFrame frame;
+
+    public Register(){
+
+        frame = new JFrame ("Registration");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(650,450));
+        frame.setResizable(false);
+
+        frame.add(Registration);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        text1.setEditable(false);
 
 
-    public Registr (JFrame parent){
-        super (parent);
-        setTitle("Create new account");
-        setContentPane(Registration);
-        setMinimumSize(new Dimension(600,500));
-        setModal(true);
-        setLocationRelativeTo(parent);
-        setVisible(true);
         btnRegister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                tfName.setText(list1.toString());
 
 
+                String inString = tfName.getText();
+                String inString2 = tfEmail.getText();
+                String inString3= tfNumber.getText();
+                String inString4 = tfCity.getText();
+
+
+                text1.setText("Your input field values are:"+"\n"+ inString + "\n"+inString2 + "\n" + inString3+"\n"+inString4);
             }
         });
     }
 
-   // private void submitData() {
-        //if (tfName.getText().equals("") || tfEmail.getText().equals("") || tfNumber.getText().equals("")){
-           // JOptionPane.showMessageDialog(this,"Please enter WORD");
-       // } else{
-            //tfName.setText(result.getText());
-            //tfEmail.setText(result.getText());
-
-
-       // }
-    //}
-
     public static void main(String[] args) {
-        Registr form = new Registr(null);
+        new Register();
     }
 
-    }
 
+    }
 
 
